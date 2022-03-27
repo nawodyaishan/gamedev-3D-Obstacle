27363f8e3d088f9848e1022a2d5b828e3b9ec40e
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-27 14:36:59 +0530

    Update

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,7 +7,7 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(1,2,2 );
     }
 
     // Update is called once per frame
