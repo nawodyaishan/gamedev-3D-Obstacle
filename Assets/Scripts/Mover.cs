89c42fa3e1f3884e1c216acc2e8f1d266ad747c6
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-04-01 17:02:11 +0530

    Added Movement and CineMachineVirtualCamera

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script for Player Object
+/// </summary>
 public class Mover : MonoBehaviour
 {
-    private float xValue = 0.0f;
-    private float yValue = 0.01f;
-    private float zValue = 0.0f;
     
+    // Speed Multiplier
+    [SerializeField]private float speedMultiplier = 10.0f;
+    
+    // Variables for moving the object
+    //[SerializeField] private float xValue = 0.0f;
+   //[SerializeField] private float yValue = 0.0f;
+    //[SerializeField] private float zValue = 0.0f;
+    
+    //private const string X_AXIS = "Horizontal";
+    //private const string Y_AXIS = "Vertical";
+    //private const string Z_AXIS = "Jump";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +28,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speedMultiplier;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speedMultiplier;
+        //float zValue = Input.GetAxis(Z_AXIS);
+        transform.Translate(xValue, 0, zValue);
     }
 }
- 
